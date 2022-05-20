580249ce127210a6cb714a444e6629e914bc017c
Dom van Almsick <dominicvana@gmail.com>
2022-05-20 17:08:34 -0500

    Chore: delete duplicate file

--- src/utils/loggers.ts
+++
@@ -1,7 +0,0 @@
-import { genLogger } from './genLogger';
-
-export const info = genLogger('info');
-export const success = genLogger('success');
-export const warn = genLogger('warn');
-export const error = genLogger('error');
-export const log = console.log;
